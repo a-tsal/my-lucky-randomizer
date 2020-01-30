542e5c9ff9fd3a85144799309c2ea2855df7663b
Alexandros Tsaloukidis <alex.tsaloukidis87@gmail.com>
2020-01-30 21:43:33 +0200

    With Classes

--- app/src/main/java/com/example/myluckyrandomizer/MainActivity.java
+++ app/src/main/java/com/example/myluckyrandomizer/MainActivity.java
@@ -16,36 +16,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     TextView txt;
     Button btn;
-    public static String randomize(int min, int max, int n, boolean replacement) {
 
-        String res = "";
-
-        if (min >= max)
-            res = "Error: The minimum possible number cannot be greater or equal to the maximum passible number";
-
-        else {
-            int length = (max - min) + 1;
-            int[] elements = new int[length];
-            if (length < n && replacement == false)
-                res = "Error: If the replacement is off, then the range of the possible numbers cannot be less than the numbers to be drawn";
-            else {
-                for (int i = 0; i < length; i++) elements[i] = min + i;
-                List<Integer> drawnList = new ArrayList<>();
-
-                for (int i = 0; i < n; i++) {
-                    int index;
-                    Random rand = new Random();
-                    index = rand.nextInt(length);
-                    if (replacement == false)
-                        while (drawnList.contains(elements[index])) index = rand.nextInt(length);
-                    drawnList.add(elements[index]);
-                    if (i == 0) res = res + drawnList.get(i);
-                    else res = res + ", " + drawnList.get(i);
-                }
-            }
-        }
-        return res;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                     resultFinal =  e.toString();
                 }
                 if (resultFinal != "") resultFinal = "Error: You must fill all the required fields with numeric descrete values";
-                else resultFinal = randomize(minimum, maximum, elements, switchState);
+                else resultFinal = Randomizer.randomize(minimum, maximum, elements, switchState);
 
 
                 txt.setText("The drawn numbers are: \n\n" + resultFinal);
--- app/src/main/java/com/example/myluckyrandomizer/Randomizer.java
+++ app/src/main/java/com/example/myluckyrandomizer/Randomizer.java
@@ -1,4 +1,39 @@
 package com.example.myluckyrandomizer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Randomizer {
+
+    public static String randomize(int min, int max, int n, boolean replacement) {
+
+        String res = "";
+
+        if (min >= max)
+            res = "Error: The minimum possible number cannot be greater or equal to the maximum passible number";
+
+        else {
+            int length = (max - min) + 1;
+            int[] elements = new int[length];
+            if (length < n && replacement == false)
+                res = "Error: If the replacement is off, then the range of the possible numbers cannot be less than the numbers to be drawn";
+            else {
+                for (int i = 0; i < length; i++) elements[i] = min + i;
+                List<Integer> drawnList = new ArrayList<>();
+
+                for (int i = 0; i < n; i++) {
+                    int index;
+                    Random rand = new Random();
+                    index = rand.nextInt(length);
+                    if (replacement == false)
+                        while (drawnList.contains(elements[index])) index = rand.nextInt(length);
+                    drawnList.add(elements[index]);
+                    if (i == 0) res = res + drawnList.get(i);
+                    else res = res + ", " + drawnList.get(i);
+                }
+            }
+        }
+        return res;
+    }
 }
